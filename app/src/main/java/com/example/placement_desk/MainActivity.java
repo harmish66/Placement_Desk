3d024284776aba3bdd9ff0e66f4c366946cebecd
harmish <harmishsejpal66500@gmail.com>
2024-02-15 22:42:41 +0530

    Acticity & layout

--- app/src/main/java/com/example/placement_desk/MainActivity.java
+++ app/src/main/java/com/example/placement_desk/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.placement_desk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.placement_desk.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    LinearLayout history, material, question, aboutus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+
+        history = findViewById(R.id.history);
+        material = findViewById(R.id.material);
+        question = findViewById(R.id.question);
+        aboutus = findViewById(R.id.aboutus);
+
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,HistoryActivity.class);
+                startActivity(i);
+            }
+        });
+        material.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MaterialActivity.class);
+                startActivity(i);
+            }
+        });
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,QuestionActivity.class);
+                startActivity(i);
+            }
+        });
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
+        databaseReference = firebaseDatabase.getReference("Data");
+        drawerLayout = findViewById(R.id.drawer);
+        findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
